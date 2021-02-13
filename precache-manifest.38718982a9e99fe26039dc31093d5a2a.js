8101e591678108855946a7b6e9a681a1e1ee833b
NOGY <norven.norov@gmail.com>
2021-02-13 21:36:35 +0800

    Updates

---
+++ precache-manifest.38718982a9e99fe26039dc31093d5a2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bdd9637fe77b8604636eb505eb01a67",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "5f70b79f190cb6f73152",
+    "url": "/burger/static/css/main.df0cac96.chunk.css"
+  },
+  {
+    "revision": "2862fdaa4fcc12e107da",
+    "url": "/burger/static/js/2.1fb0107f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger/static/js/2.1fb0107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f70b79f190cb6f73152",
+    "url": "/burger/static/js/main.bc4ebd38.chunk.js"
+  },
+  {
+    "revision": "b70cca1f39cb2c1dd795",
+    "url": "/burger/static/js/runtime-main.95ddc443.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
